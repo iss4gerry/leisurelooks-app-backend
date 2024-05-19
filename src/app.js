7392d5e51cd725336db66f87827adca0e673b954
gerry <gerrydesrian48@gmail.com>
2024-05-19 12:32:07 +0700

    route

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const router = require('./routes/index')
 const app = express()
 
 app.use(express.json())
@@ -7,5 +8,7 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+
+app.use(router)
 
 module.exports = app
---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,11 @@
+const httpStatus = require('http-status')
+
+const register = async (req, res) => {
+    res.send('OK')
+}
+
+const login = async (req, res) => {
+    res.send('OK')
+}
+
+module.exports = { register, login }
