79fc857b6850a01f155ff3aea669122b04c2ced2
gerry <gerrydesrian48@gmail.com>
2024-05-21 19:11:46 +0700

    install morgan

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = require('./routes/index')
 const errorHandler = require('./middlewares/error')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
 
 app.get('/', (req, res) => {
     res.send('Hello World')
