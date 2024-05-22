ccdac7f4a3fcaf1ce39ce0567d890f5f1c63746d
Gilang <gilangnurrizki173@gmail.com>
2024-05-22 14:24:55 +0700

    install cors

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const router = require('./routes/index')
-const errorHandler = require('./middlewares/error')
-const app = express()
+const express = require("express");
+const router = require("./routes/index");
+const errorHandler = require("./middlewares/error");
+const cors = require("cors");
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.use(cors());
+app.use("*", cors());
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.use(router)
-app.use(errorHandler)
+app.use(router);
+app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
